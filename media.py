8ade8b5b87060676e499af8b2a7fecedd80028d2
EMERSON CARLOS <166720282+emersoncarlos-dev@users.noreply.github.com>
2026-06-18 16:06:38 -0300

    Calculo média

---
+++ media.py
@@ -0,0 +1,14 @@
+print("--- Cálculo de Média Simples ---")
+
+# Inputs dos números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+# Cálculo da média
+media = (num1 + num2 + num3) / 3
+
+# Resultado
+print("-" * 30)
+print(f"A média dos números é: {media}")
+print("-" * 30)
