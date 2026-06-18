f66283a131f581c30ed72ce96fb9d12384404002
Beatriz <beamedeiros82@gmail.com>
2026-06-18 16:01:39 -0300

    refactor: Nova versão

--- subtracao.py
+++ subtracao.py
@@ -1,2 +1,5 @@
-def subtrair(a, b):
-    return a - b
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = (num1 - num2)
+print(resultado)
